2d3b9a6f6d6a0815e4af726f9dbe7e8fb965e0a9
alt1tla <GerdzhikovaVeronWench@gmail.com>
2023-12-22 16:48:02 +0300

    alets added

--- EcoSorter/store/forms.py
+++ EcoSorter/store/forms.py
@@ -3,8 +3,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django import forms
 
 class SignUpForm(UserCreationForm):
-    email = forms.EmailField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Email Address'}))
-
+    email = forms.EmailField(label="Адрес электронной почты", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'motte_vessale@mail.ru'}))
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
@@ -15,19 +14,14 @@ class SignUpForm(UserCreationForm):
         self.fields['username'].widget.attrs['class'] = 'form-control'
         self.fields['username'].widget.attrs['placeholder'] = 'Matvei'
         self.fields['username'].label = 'Имя пользователя'
-        self.fields['username'].help_text = '<span class="form-text text-muted"><small>Обязательно*</small></span>'
-
-        self.fields['email'].widget.attrs['class'] = 'form-control'
-        self.fields['email'].widget.attrs['placeholder'] = 'motte_vessale@mail.ru'
-        self.fields['email'].label = 'Адрес электронной почты'
-        self.fields['email'].help_text = '<span class="form-text text-muted"><small>Обязательно*</small></span>'
+        self.fields['username'].help_text = '<span class="form-text text-muted"><small>*Не более 150 символов. Только буквы, цифры и @/./+/-/_.</small></span>'
 
         self.fields['password1'].widget.attrs['class'] = 'form-control'
         self.fields['password1'].widget.attrs['placeholder'] = 'Veselove123'
         self.fields['password1'].label = 'Пароль'
-        self.fields['password1'].help_text = '<span class="form-text text-muted"><small>Обязательно*</small></span>'
+        self.fields['password1'].help_text = '<ul class="form-text text-muted small"><li>Ваш пароль не должен быть слишком похож на другую вашу личную информацию.</li><li>Ваш пароль должен содержать не менее 8 символов.</li><li>Ваш пароль не может быть часто используемым паролем.</li><li>Ваш пароль не может быть полностью цифровым.</li></ul>'
 
         self.fields['password2'].widget.attrs['class'] = 'form-control'
         self.fields['password2'].widget.attrs['placeholder'] = 'Veselove123'
         self.fields['password2'].label = 'Подтверждение пароля'
-        self.fields['password2'].help_text = '<span class="form-text text-muted"><small>Обязательно*</small></span>'
+        self.fields['password2'].help_text = '<span class="form-text text-muted"><small>*Введите тот же пароль, что и раньше, для проверки.</small></span>'
--- EcoSorter/store/views.py
+++ EcoSorter/store/views.py
@@ -45,13 +45,13 @@ def logout_user(request):
 
 
 def register_user(request):
-    form = SignUpForm
+    form = SignUpForm()
     if request.method == "POST":
         form = SignUpForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data["username"]
-            password = form.cleaned_data["password"]
+            password = form.cleaned_data["password1"]
             user = authenticate(username=username,password=password)
             login(request,user)
             messages.success(request,("Регистрация прошла успешно"))

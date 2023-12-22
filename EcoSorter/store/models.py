c2532def05711273df096f1634c0b4d1c5437c04
alt1tla <GerdzhikovaVeronWench@gmail.com>
2023-12-22 18:43:56 +0300

    lol3

--- EcoSorter/store/models.py
+++ EcoSorter/store/models.py
@@ -10,7 +10,7 @@ class Category(models.Model):
 
 class Customer(models.Model):
     first_name = models.CharField(max_length=50)
-    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
     phone = models.CharField(max_length=11)
     email = models.EmailField(max_length=100)
     password = models.CharField(max_length=50)

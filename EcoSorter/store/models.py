aae503701d23041d876cf15d4f57cf1b07c96d34
alt1tla <GerdzhikovaVeronWench@gmail.com>
2024-01-10 09:59:52 +0300

    dropped link to navbar addded

--- EcoSorter/store/models.py
+++ EcoSorter/store/models.py
@@ -16,7 +16,7 @@ class Customer(models.Model):
     password = models.CharField(max_length=50)
 
     def __str__(self):
-        return f'{self.first_name} + {self.last_name}'
+        return f'{self.first_name} {self.last_name}'
 
 class Product(models.Model):
     name = models.CharField(max_length=50)
